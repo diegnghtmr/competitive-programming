a257aeac0615bb7cca1249eb026a2fa422577fd9
diegnghtmr <diegoa.floresq@uqvirtual.edu.co>
2025-03-22 18:58:18 -0500

    Ejercicio 1124

---
+++ uHunt/CelebrityJeopardy.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int main() {
+    string ecuacion = "";
+    while(getline(cin, ecuacion)) {
+        cout<<ecuacion<<endl;
+    }
+
+
+    return 0;
+}
+
